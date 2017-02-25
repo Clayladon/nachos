5830a70a0bb16cf01e181a126b302006a1f9cf9e
Clayladon <renaulk@hotmail.com>
2017-02-24 19:29:30 -0500

    Added comments

--- nachos/threads/Condition2.java
+++ nachos/threads/Condition2.java
@@ -23,8 +23,8 @@ public class Condition2 {
      *				<tt>wake()</tt>, or <tt>wakeAll()</tt>.
      */
     public Condition2(Lock conditionLock) {
-	this.conditionLock = conditionLock;
-	waitQueue = new LinkedBlockingQueue<KThread>();
+		this.conditionLock = conditionLock;
+		waitQueue = new LinkedBlockingQueue<KThread>();
     }
 
     /**
@@ -34,16 +34,21 @@ public class Condition2 {
      * automatically reacquire the lock before <tt>sleep()</tt> returns.
      */
     public void sleep() {
+    	//Ensure that the current thread holds the lock
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
 	
+		//Store interrupts and release the lock
 		boolean interruptStatus = Machine.interrupt().disable();
 		conditionLock.release();
 
+		//Add the current thread to the waitQueue and put it to sleep
 		waitQueue.add(KThread.currentThread());
 		KThread.sleep();
 	
+		//After waking up, reacquire the lock
 		conditionLock.acquire();
 	
+		//Restore interrupts
 		Machine.interrupt().restore(interruptStatus);
     }
 
@@ -52,18 +57,24 @@ public class Condition2 {
      * current thread must hold the associated lock.
      */
     public void wake() {
+    	//Ensure that the current thread holds the lock
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
 	
+		//Store interrupts
 		boolean interruptStatus = Machine.interrupt().disable();
 		
+		//If the wait queue is not empty
 		if(waitQueue.peek() != null)
 			try{
+				//Take the head off the queue and ready it
 				((KThread)waitQueue.take()).ready();
 			}
 			catch(InterruptedException e){
+				//This operation is atomic and this error will never happen
 				System.out.println("Error waking thread.");
 			}
 		
+		//Restore interrupts
 		Machine.interrupt().restore(interruptStatus);
     }
 
@@ -72,24 +83,34 @@ public class Condition2 {
      * thread must hold the associated lock.
      */
     public void wakeAll() {
+    	//Ensure that the current thread holds the lock
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
 	
+		//Store interrupts
 		boolean interruptStatus = Machine.interrupt().disable();
 		
+		//While the wait queue is not empty
 		while(waitQueue.peek() != null)
 			try{
+				//Take the head off the queue and ready it
 				((KThread)waitQueue.take()).ready();
 			}
 			catch(InterruptedException e){
+				//This operation is atomic and this error will never happen
 				System.out.println("Error waking thread.");
 			}
 			
+		//Restore interrupts
 		Machine.interrupt().restore(interruptStatus);
     }
 
+	/**
+	 * Tests whether this module is working.
+	 */
     public static void selfTest(){
 		Lib.debug(Condition2TestChar, "Condition2.selfTest(): Starting self test.");
 		
+		//Create a lock and Condition2 object to test with
 		Lock lock = new Lock();
 		Condition2 tester = new Condition2(lock);
 		
@@ -100,6 +121,7 @@ public class Condition2 {
 		KThread waker = new KThread();
 		
 		Lib.debug(Condition2TestChar, "Condition2.selfTest(): Two threads created, sleeper and waker.");
+		//Set sleeper's target to acquire the lock, fork a waker, sleep, and release the lock on waking up
 		sleeper.setTarget(new Runnable(){
 			public void run(){
 				Lib.debug(Condition2TestChar, "Condition2.selfTest(): Sleeper starting.");
@@ -113,6 +135,8 @@ public class Condition2 {
 				lock.release();	
 				}
 			});
+		//Set waker's target to acquire the lock, and print the state of the wait queue before and after
+		//cycling the tester's wait queue.
 		waker.setTarget(new Runnable(){
 			public void run(){
 
@@ -135,18 +159,19 @@ public class Condition2 {
 					Lib.debug(Condition2TestChar, "Condition2.selfTest(): Tester's waitQueue is not empty");
 
 
-				tester.wake();	//Testing if wake can handle an empty
-								//waitQueue	
+				tester.wake();	//Testing if wake can handle an empty wait queue
 				lock.release();
 			}
 		});
 			sleeper.fork();
 			sleeper.join();
 
+
 		//Testing multiple sleeps & the wakeAll method
 
-
 		Lib.debug(Condition2TestChar, "Condition2.selfTest(): Starting multiple wake test.");
+		
+		//Create three sleeper threads and a waker thread
 		KThread sleep1 = new KThread();
 		KThread sleep2 = new KThread();
 		KThread sleep3 = new KThread();
@@ -154,6 +179,7 @@ public class Condition2 {
 		Lib.debug(Condition2TestChar, "Condition2.selfTest(): Three threads (sleep1, sleep2, sleep3)"
 										+" and thread (wakeMulti) created.");
 	
+		//Set sleeper targets to sleep when forked and fork the next sleeper
 		sleep1.setTarget(new Runnable(){
 			public void run(){
 					lock.acquire();
@@ -174,6 +200,7 @@ public class Condition2 {
 					lock.release();	
 				}
 			});
+		//Sleep3 forks wakeMulti instead of another sleeper thread
 		sleep3.setTarget(new Runnable(){
 			public void run(){
 					lock.acquire();
@@ -185,6 +212,7 @@ public class Condition2 {
 					lock.release();	
 				}
 			});
+		//WakeMulti calls the wakeAll() method to wake all three sleepers
 		wakeMulti.setTarget(new Runnable(){
 			public void run(){
 				lock.acquire();
@@ -203,6 +231,7 @@ public class Condition2 {
 		Lib.debug(Condition2TestChar, "Condiiton2.selfTest(): Finished selfTest(), passed.");
     }
 
+	//Datafields
     private Lock conditionLock;
     private LinkedBlockingQueue<KThread> waitQueue;
     private static final char Condition2TestChar = 'c';
