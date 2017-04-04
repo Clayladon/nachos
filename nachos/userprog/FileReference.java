3588836fe71d5c93af3ecd47befd00891497efbb
Clayladon <renaulk@hotmail.com>
2017-04-04 18:47:08 -0400

    Added comments to FileReference.java

--- nachos/userprog/FileReference.java
+++ nachos/userprog/FileReference.java
@@ -1,13 +1,29 @@
 package nachos.userprog;
 
+/**
+ * This class represents a reference to a file. The purpose is to keep track of each
+ * process' opened giles as well as maintain a global view of all opened files and their
+ * references.
+ */
 public class FileReference{
+
+	//The number of references associated to a file
 	public int numReferences;
+
+	//Whether or not a file is set to be unlinked
 	public boolean markedForDeath;
+
+	//The name of the file
 	public String fileName;
 	
+	//Constructor
 	public FileReference(String inFileName){
+		//Since the FileReference is being created 1 reference must exist
 		numReferences = 1;
+		
+		//The file is not marked for unlinking on creation
 		markedForDeath = false;
+		
 		fileName = inFileName;
 	}
 }
