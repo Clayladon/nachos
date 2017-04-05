806c066d7b2877d8939da4b5d40abd1e9564a072
EvilPineapple93 <hmarceau@laurentian.ca>
2017-04-05 18:39:49 -0400

    TESTS4 update

--- nachos/test/TESTS4.c
+++ nachos/test/TESTS4.c
@@ -8,6 +8,12 @@ int main(){
 	char fname[] = {'h','e','l','l','o','.','c','o','f','f','\0'};
 	char *fptr = &fname[0];
 	int num = exec(fptr,0,null);
+
+	int status = 0;
+	int *sts = &status;
+	join(num,sts);
+
+	printf("Success\n");
 	
 	return 0;
 }
