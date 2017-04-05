6c8b2c317efaf7d4bfdd967f79e49ce33ab4972d
EvilPineapple93 <hmarceau@laurentian.ca>
2017-04-04 22:31:41 -0400

    Created Task 1 testcase file

---
+++ nachos/test/TESTS1.c
@@ -0,0 +1,33 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "syscall.h"
+
+int main(){
+
+	char filename[] = {'I','m','a','g','i','n','a','r','y'};
+	char *fptr = &filename[0];
+
+	//Imaginary is not an existing file
+	int fd = open(fptr);
+
+	if(fd != -1){
+		printf("Failure\n");
+		exit(0);
+	}
+
+	//Create Imaginary
+	fd = creat(fptr);
+
+	if(fd == -1){
+		printf("Failure\n");
+		exit(0);
+	}
+
+	//Open Imaginary, compare File Descriptor to fd
+
+	int ofd = open(fptr);
+
+	printf("fd = %d\nofd = %d\n", fd, ofd);
+
+	return 0;
+}
