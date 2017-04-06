fb43fcfafb0ff04da734317d7f61e94d6ba0f220
Clayladon <renaulk@hotmail.com>
2017-04-06 15:20:17 -0400

    Commented ChildProcess.java

--- nachos/userprog/ChildProcess.java
+++ nachos/userprog/ChildProcess.java
@@ -1,9 +1,16 @@
 package nachos.userprog;
 
+/**
+ * This class ensures that the child process can keep track of the children 
+ * and their exit values. To this it identifies the child's status (returnValue) 
+ * and the process it is running.
+ */
 public class ChildProcess{
+	//Datafields
 	public int returnValue;
 	public UserProcess process;
 	
+	//Constructor
 	public ChildProcess(UserProcess child){
 		process = child;
 		returnValue = -1;
