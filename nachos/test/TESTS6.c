f5f1d902d9ee74c5424fe369b0a25d46365e2ce9
Kaitlynn Anderson-Butcher <k.anderson-butcher@hotmail.com>
2017-04-06 00:29:39 -0400

    adfsd

--- nachos/test/TESTS6.c
+++ nachos/test/TESTS6.c
@@ -13,16 +13,16 @@ int main(){
 	char fileName2[] = "hello.c";
 	char *fptr2 = &fileName2[0];
 
-	int b = exec(fptr2,-1, null);
-	int c = exec(fptr2,5, argv);
+//	int b = exec(fptr2,-1, null);
+//	int c = exec(fptr2,5, argv);
 
 	join(a,null);
-	join(b,null);
-	join(c,null);
+//	join(b,null);
+//	join(c,null);
 
 	printf("Trying to Exec non existent file: %d\n", a);
-	printf("Trying to Exec with improper argc: %d\n", b);
-	printf("Trying to Exec with mismatched argc, argv: %d\n", c);
+//	printf("Trying to Exec with improper argc: %d\n", b);
+//	printf("Trying to Exec with mismatched argc, argv: %d\n", c);
 
 	return 0;
 }
