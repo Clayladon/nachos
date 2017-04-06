118b8e5ba20a4a9ada30dc7d6235e848a47b419a
EvilPineapple93 <hmarceau@laurentian.ca>
2017-04-06 00:01:44 -0400

    sdasdadasd

--- nachos/test/joinB.c
+++ nachos/test/joinB.c
@@ -7,6 +7,8 @@ int main(int argc, char *argv[]){
 	char argument = *argv[0];
 
 	printf("B attempting to join non-child process A\n");
+
+	printf("A's supposed PID: %d\n", (int)(argument - '0'));
 	join((int)(argument - '0'),null);
 	exit(127);
 	return 0;
