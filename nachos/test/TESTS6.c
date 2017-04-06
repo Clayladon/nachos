b4f426f68019359943255bc342cd67655f5765b7
Kaitlynn Anderson-Butcher <k.anderson-butcher@hotmail.com>
2017-04-06 00:35:15 -0400

    dkdkfdkkdkd

--- nachos/test/TESTS6.c
+++ nachos/test/TESTS6.c
@@ -14,15 +14,15 @@ int main(){
 	char *fptr2 = &fileName2[0];
 
 	int b = exec(fptr2,-1, null);
-//	int c = exec(fptr2,5, argv);
+	int c = exec(fptr2,5, argv);
 
 	join(a,null);
 	join(b,null);
-//	join(c,null);
+	join(c,null);
 
 	printf("Trying to Exec non existent file: %d\n", a);
-	printf("Trying to Exec with improper argc: %d\n", b);
-//	printf("Trying to Exec with mismatched argc, argv: %d\n", c);
+	printf("Trying to Exec with negative argc: %d\n", b);
+	printf("Trying to Exec with mismatched argc, argv: %d\n", c);
 
 	return 0;
 }
