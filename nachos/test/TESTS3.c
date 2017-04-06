8a982d0c639f1eb2130c480abcd0fea2337e93da
EvilPineapple93 <hmarceau@laurentian.ca>
2017-04-06 16:33:58 -0400

    TESTS3

--- nachos/test/TESTS3.c
+++ nachos/test/TESTS3.c
@@ -4,18 +4,12 @@
 
 int main(){
 
-	//char filename[] = {'F','i','c','t','i','o','n','a','l'};
-	//char *fPtr = &filename[0];
-
-//	int fd = open(fPtr);
-//	printf("File descriptor: %d\n", fd);
 	char buffer[24];
 	char *buf = &buffer[0];
 
 	int bytesRead = read(4, buf, 24);
 
 	printf("Bytes read: %d\n", bytesRead);
-	printf("Contents of text.txt:\n%s\n", buffer);
 
 	char buffer2[] = {'H','E','L','L','O',' ','W','O','R','L','D','!','\n'};
 	char *buf2 = &buffer2[0];
