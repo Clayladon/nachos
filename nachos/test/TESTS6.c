56eb776963962fe76edd5022c0020db906506cee
EvilPineapple93 <hmarceau@laurentian.ca>
2017-04-06 00:25:52 -0400

    sdad

--- nachos/test/TESTS6.c
+++ nachos/test/TESTS6.c
@@ -6,11 +6,23 @@ int main(){
 
 	char fileName[] = "Imaginary";
 	char *fptr = &fileName[0];
-	
 	char *argv[3] = {};
-	printf("Trying to Exec non existent file: %d\n", exec(fptr,0,null));
-	printf("Trying to Exec with improper argc: %d\n", exec(fptr,-1,null));
-	printf("Trying to Exec with mismatched argc, argv: %d\n", exec(fptr,5,argv));
+
+	int a = exec(fptr,0,null);
+
+	fileName[] = "hello.c";
+	fptr = &fileName[0];
+
+	int b = exec(fptr,-1, null);
+	int c = exec(fptr,5, argv);
+
+	join(a,null);
+	join(b,null);
+	join(c,null);
+
+	printf("Trying to Exec non existent file: %d\n", a);
+	printf("Trying to Exec with improper argc: %d\n", b);
+	printf("Trying to Exec with mismatched argc, argv: %d\n", c);
 
 	return 0;
 }
