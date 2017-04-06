c9b4a7149e02304e3fbecf53a8e7bf854949b93e
EvilPineapple93 <hmarceau@laurentian.ca>
2017-04-06 00:08:21 -0400

    Finished TESTS5.c

--- nachos/test/joinB.c
+++ nachos/test/joinB.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]){
 	char argument = *argv[0];
 
 	printf("B attempting to join non-child process A\n");
-	printf("Join attempt's return code: %d", join((int)(argument), null));
+	printf("Join attempt's return code: %d\n", join((int)(argument), null));
 	exit(127);
 	return 0;
 }
